8d6aec75d807ef419a00a899acc14fd2e9ab0ddc
khalidhersi <cipherpolfoundation@gmail.com>
2022-02-25 18:03:27 +0000

    started springboot backend for mux livestreamer

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StyleSheet, View } from 'react-native';
 import Home from "./Screens/Home/Home";
+import BroadcastScreen from './Screens/BroadcastScreen/BroadcastScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <BroadcastScreen/>
       <Home/>
     </View>
   );
---
+++ Rant-SpingBoot/Rant-SpingBoot/src/main/java/com/example/RantSpingBoot/LiveStream.java
@@ -0,0 +1,2 @@
+package com.example.RantSpingBoot;public class LiveStream {
+}
